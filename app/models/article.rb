1859d71df024c7bcaf18bcee9142c1a57f5299ca
SahilThakur7 <sahilrd07@gmail.com>
2018-10-10 09:05:39 +0530

    added the controllers and wrote the initial model tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
     belongs_to :category
+
+    validates :title, presence: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    #associtation tests
+    
+    it { should have_many(:articles) }
+
+    #validation tests
+
+    it {should validate_presence_of(:title)}
 end

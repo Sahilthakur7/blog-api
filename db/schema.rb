04615db3943b986dd505bebd17775439c65eb4b2
SahilThakur7 <sahilrd07@gmail.com>
2018-10-10 08:56:19 +0530

    added content field to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_113856) do
+ActiveRecord::Schema.define(version: 2018_10_10_032544) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_113856) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.text "content"
     t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
